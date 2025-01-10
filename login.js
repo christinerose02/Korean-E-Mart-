068d64ccecb947c0a2eb99eda3123a6d209a80b3
christinerose02 <cathlianaroseceallorando@gmail.com>
2025-01-10 22:28:59 +0800

    html css files

---
+++ login.js
@@ -0,0 +1,7 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+$(document).ready(function(){
+    $("#login").click(function(){
+    alert("Congratulations! LOG-IN BUTTON was clicked!");
+});
+});
